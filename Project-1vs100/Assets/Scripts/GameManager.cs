872df09d9caf234bb1c2fdaa44a7f56ff07e8c35
thea-t <58753010+thea-t@users.noreply.github.com>
2021-04-14 18:15:31 +0300

    Essentials

---
+++ Project-1vs100/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } // static singleton
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
+    
+
+}
