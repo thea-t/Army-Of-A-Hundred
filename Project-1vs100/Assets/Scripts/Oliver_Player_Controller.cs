dc6d24bc61dd87b6de12ba0134dd3b79d8d131ce
oliverc2550 <60927456+oliverc2550@users.noreply.github.com>
2021-04-23 13:02:14 +0100

    Script Rename

---
+++ Project-1vs100/Assets/Scripts/Oliver_Player_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oliver_Player_Controller : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject fireBallEffect;
+    public float fireBallRadius = 5f;
+    public float fireBallForce = 700f;
+    public GameObject gravitySinkHoleEffect;
+    public float gravitySinkHoleRadius = 5f;
+    public float gravitySinkHoleForce = 700f;
+    private void Fireball(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(fireBallEffect, position, rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(position, fireBallRadius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            nearbyObject.GetComponent<Oliver_RagdollToggle>().isWalking = false;
+            nearbyObject.GetComponent<Oliver_RagdollToggle>().ToggleRagdoll(true);
+        }
+
+        Collider[] collidersToExplode = Physics.OverlapSphere(position, fireBallRadius);
+        foreach (Collider nearbyObject in collidersToExplode)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(fireBallForce, position, fireBallRadius);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Fireball(hit.transform.position, hit.transform.rotation);
+        }
+    }
+}
---
+++ Project-1vs100/Assets/Scripts/Oliver_Spell_List.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oliver_Spell_List : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject fireBallEffect;
+    public float fireBallRadius = 5f;
+    public float fireBallForce = 700f;
+    public GameObject gravitySinkHoleEffect;
+    public float gravitySinkHoleRadius = 5f;
+    public float gravitySinkHoleForce = 700f;
+    private void Fireball(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(fireBallEffect, position, rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(position, fireBallRadius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            nearbyObject.GetComponent<Oliver_RagdollToggle>().isWalking = false;
+            nearbyObject.GetComponent<Oliver_RagdollToggle>().ToggleRagdoll(true);
+        }
+
+        Collider[] collidersToExplode = Physics.OverlapSphere(position, fireBallRadius);
+        foreach (Collider nearbyObject in collidersToExplode)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(fireBallForce, position, fireBallRadius);
+            }
+        }
+    }
+}
