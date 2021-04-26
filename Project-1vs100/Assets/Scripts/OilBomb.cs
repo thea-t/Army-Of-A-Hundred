c03fe47a4980f7cfac668bda7a9b05c7d0215c90
CS50Bru <50418228+CS50Bru@users.noreply.github.com>
2021-04-26 07:58:33 +0100

    have added an explosion prefab, but it's not doing anything atm...

---
+++ Project-1vs100/Assets/Scripts/OilBomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilBomb : MonoBehaviour
+{
+    [SerializeField]
+    float m_impactThreshold;
+    [SerializeField]
+    GameObject m_bigExplosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float collisionForce = collision.relativeVelocity.magnitude;
+        if (collisionForce > m_impactThreshold)
+        {
+            Debug.Log("I'm gonna blow up" + collisionForce);
+            Instantiate(m_bigExplosion, transform);
+        }
+    }
+}
